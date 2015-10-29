5c7233fef4b08702ae04ed457fbbb4cbfda65677
Eeo Jun <packwolf58@gmail.com>
2015-10-29 16:09:24 +0800

    Fix errors

--- simplecoro.py
+++ simplecoro.py
@@ -2,10 +2,14 @@ from contextlib import contextmanager
 from threading import current_thread
 
 
+class WrongThread(Exception):
+    pass
+
+
 @contextmanager
 def guard(thread):
     if current_thread() != thread:
-        raise Exception
+        raise WrongThread
     yield
 
 
@@ -13,10 +17,11 @@ class Fiber(object):
     def __init__(self, task):
         self.coro = task()
         self.thread = current_thread()
-        self.guard = ThreadGuard(self.thread)
 
     def switch(self):
         with guard(self.thread):
             for fiber in self.coro:
+                if fiber is None:
+                    break
                 fiber.switch()
                 return
