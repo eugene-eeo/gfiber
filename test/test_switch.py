59e9b47974bfa22abf202e04796a97556a8cdb03
Eeo Jun <packwolf58@gmail.com>
2015-11-06 20:18:38 +0800

    Add spec for switching to multiple fibers

--- test/test_switch.py
+++ test/test_switch.py
@@ -53,3 +53,21 @@ def test_switch_from_different_thread():
     thread = ExThread(target=assertion)
     thread.start()
     thread.join()
+
+
+def test_switch_multiple():
+    def t1():
+        yield f2
+        yield f2
+
+    def t2():
+        arr.append(1)
+        yield f1
+        arr.append(2)
+
+    arr = []
+    f1 = Fiber(t1)
+    f2 = Fiber(t2)
+    f1.switch()
+    assert arr == [1,2]
+    assert f2.done
